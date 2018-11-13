d284080c0c31bb184b7ae35d958b2616465ecd04
Gabriel Carneiro <gabriel.carneiro97@live.com>
2018-11-13 14:58:50 -0200

    Tá uma merda, daquia pouco eu arrumo esse trem

--- js/Clock.js
+++ js/Clock.js
@@ -1,20 +1,36 @@
 class Clock {
   constructor(horaId, minutoId, segundoId, diaId, mesId) {
-    this.horaDomElement = dom.byId(horaId);
-    this.minutoDomElement = dom.byId(minutoId);
-    this.segundoDomElement = dom.byId(segundoId);
-    this.diaDomElement = dom.byId(diaId);
-    this.mesDomElement = dom.byId(mesId);
+    this.horaDomEl = dom.byId(horaId);
+    this.minutoDomEl = dom.byId(minutoId);
+    this.segundoDomEl = dom.byId(segundoId);
+
+    this.horaPonteiroDomEl = dom.byId(`${horaId}-ponteiro`);
+    this.minutoPonteiroDomEl = dom.byId(`${minutoId}-ponteiro`);
+
+    this.diaDomEl = dom.byId(diaId);
+    this.mesDomEl = dom.byId(mesId);
+  }
+
+  updateTime() {
+    var datetime = tizen.time.getCurrentDateTime(),
+      hour = datetime.getHours(),
+      minute = datetime.getMinutes(),
+      second = datetime.getSeconds();
+
+    rotateElement(this.horaPonteiroDomEl, (hour + (minute / 60) + (second / 3600)) * 30);
+    rotateElement(this.minutoPonteiroDomEl, (minute + second / 60) * 6);
   }
 
   tic() {
     return new Promise((resolve) => {
       var datetime = tizen.time.getCurrentDateTime();
-      html(this.horaDomElement, pad(datetime.getHours()));
-      html(this.minutoDomElement, pad(datetime.getMinutes()));
-      html(this.segundoDomElement, pad(datetime.getSeconds()));
-      html(this.diaDomElement, pad(datetime.getDate()));
-      html(this.mesDomElement, pad(datetime.getMonth() + 1));
+      html(this.horaDomEl, pad(datetime.getHours()));
+      html(this.minutoDomEl, pad(datetime.getMinutes()));
+      html(this.segundoDomEl, pad(datetime.getSeconds()));
+      html(this.diaDomEl, pad(datetime.getDate()));
+      html(this.mesDomEl, pad(datetime.getMonth() + 1));
+
+      // this.updateTime();
       
       resolve();
     });
