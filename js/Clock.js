bff36d10cc2bc07d83eb909f2db3b861eecbdd9a
Gabriel Carneiro <gabriel.carneiro97@live.com>
2018-11-14 12:32:59 -0200

    Agora as bolhas começaram a fazer sentido

--- js/Clock.js
+++ js/Clock.js
@@ -4,33 +4,52 @@ class Clock {
     this.minutoDomEl = dom.byId(minutoId);
     this.segundoDomEl = dom.byId(segundoId);
 
-    this.horaPonteiroDomEl = dom.byId(`${horaId}-ponteiro`);
-    this.minutoPonteiroDomEl = dom.byId(`${minutoId}-ponteiro`);
+    this.bigDomEl = dom.byId('big');
+    this.bigBubbles = Array.from(dom.byClass('big bubble'));
+
+    this.medDomEl = dom.byId('med');
+    this.medBubbles = Array.from(dom.byClass('med bubble'));
 
     this.diaDomEl = dom.byId(diaId);
     this.mesDomEl = dom.byId(mesId);
   }
 
   updateTime() {
-    var datetime = tizen.time.getCurrentDateTime(),
-      hour = datetime.getHours(),
-      minute = datetime.getMinutes(),
-      second = datetime.getSeconds();
+    const datetime = tizen.time.getCurrentDateTime();
 
-    rotateElement(this.horaPonteiroDomEl, (hour + (minute / 60) + (second / 3600)) * 30);
-    rotateElement(this.minutoPonteiroDomEl, (minute + second / 60) * 6);
+    const hora = datetime.getHours();
+    const minuto = datetime.getMinutes();
+    const segundo = datetime.getSeconds();
+
+    const dia = datetime.getDate();
+    const mes = datetime.getMonth() + 1;
+
+    html(this.horaDomEl, pad(hora));
+    html(this.minutoDomEl, pad(minuto));
+    html(this.segundoDomEl, pad(segundo));
+
+    html(this.diaDomEl, pad(dia));
+    html(this.mesDomEl, pad(mes));
+
+    const bigRotate = ((hora + (minuto / 60) + (segundo / 3600)) * 30) - 90;
+    const bigRotateRev = -(bigRotate);
+
+    const medRotate = ((minuto + segundo / 60) * 6) - 90;
+    const medRotateRev = -(medRotate);
+
+
+    rotateElement(this.bigDomEl, bigRotate);
+
+    this.bigBubbles.forEach(el => rotateElement(el, bigRotateRev));
+
+    rotateElement(this.medDomEl, medRotate);
+
+    this.medBubbles.forEach(el => rotateElement(el, medRotateRev));
   }
 
   tic() {
     return new Promise((resolve) => {
-      var datetime = tizen.time.getCurrentDateTime();
-      html(this.horaDomEl, pad(datetime.getHours()));
-      html(this.minutoDomEl, pad(datetime.getMinutes()));
-      html(this.segundoDomEl, pad(datetime.getSeconds()));
-      html(this.diaDomEl, pad(datetime.getDate()));
-      html(this.mesDomEl, pad(datetime.getMonth() + 1));
-
-      // this.updateTime();
+      this.updateTime();
       
       resolve();
     });
